d3f643d6aae33e026185d1eb429f6a89e09da71f
Tolotra <tolotor@yahoo.fr>
2022-03-09 22:59:51 +0300

    Database setups bootstrap in api server starter

--- src/apiServer.ts
+++ src/apiServer.ts
@@ -3,8 +3,14 @@ import http from "http";
 // Express rest api app
 import app from "./app";
 
+// Database setups bootstrapper
+import bootstrapDb from "./db";
+
 // HTTP server
 const server = http.createServer(app);
+
+// Bootstrapping database setups
+bootstrapDb();
 
 // Listening to a port
 const PORT: number | string = process.env.PORT || 5000;
