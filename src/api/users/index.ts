86584b5dc8e3709fcc968c59219d42e272c1b5b9
Tolotra <tolotor@yahoo.fr>
2022-03-11 22:38:25 +0300

    Exporting user model related stuffs from the users api entry point

---
+++ src/api/users/index.ts
@@ -0,0 +1,4 @@
+// User model
+import UserModel, { UserAttributes, UserCreationAttributes, userModelSchema } from "./user-model";
+
+export { UserModel, UserAttributes, UserCreationAttributes, userModelSchema };
