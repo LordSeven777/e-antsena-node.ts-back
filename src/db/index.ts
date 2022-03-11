2532908c4efe8344bf11d1fc9d4c285fc72c37f7
Tolotra <tolotor@yahoo.fr>
2022-03-11 22:40:36 +0300

    Updating the models syncer and including a sync option to the db bootstrapper

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,17 @@
 // Sequelize database connection instance
 import sequelize from "./connection";
 
+// The models syncer
+import syncModelsWithDb from "./syncing";
+
+// Interface for the synchronization option
+interface SyncOption {
+    force?: boolean;
+    alter?: boolean;
+}
+
 // Bootstraps the database setups
-const bootstrapDb = async (withSeeding: boolean = false) => {
+const bootstrapDb = async (withSeeding: boolean = false, syncOption: SyncOption = { force: false }) => {
     // Authenticating with the database
     try {
         await sequelize.authenticate();
@@ -11,6 +20,18 @@ const bootstrapDb = async (withSeeding: boolean = false) => {
     catch (error: any) {
         console.log('Failed to connect to MySQL database ...');
         console.log(error.message);
+    }
+
+    if (syncOption?.force || syncOption?.alter) {
+        // Syncing with the database
+        try {
+            await syncModelsWithDb(syncOption);
+            console.log('Resynced with the MySQL database ...');
+        }
+        catch (error: any) {
+            console.log('Failed to resync with MySQL database ...');
+            console.log(error.message);  
+        }
     }
 }
 
