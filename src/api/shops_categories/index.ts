9b040f0f36075f5186083c3be928611d5554279b
Tolotra <tolotor@yahoo.fr>
2022-03-12 20:47:53 +0300

    Export of the ShopsCategories model and mock data

---
+++ src/api/shops_categories/index.ts
@@ -0,0 +1,15 @@
+// ShopsCategories model
+import ShopsCategoriesModel, {
+    ShopsCategoriesAttributes,
+    shopsCategoriesModelSchema
+} from "./shops_categories-model";
+
+// Mock data for the shops_categories rel
+import shopsCategoriesMock from "./shops_categories-mock";
+
+export {
+    ShopsCategoriesModel,
+    ShopsCategoriesAttributes,
+    shopsCategoriesModelSchema,
+    shopsCategoriesMock
+};
