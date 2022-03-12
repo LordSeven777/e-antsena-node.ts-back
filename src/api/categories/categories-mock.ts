857aec2ebddb444b75b5f6d3f86115f5423f7965
Tolotra <tolotor@yahoo.fr>
2022-03-12 19:24:26 +0300

    Mock data for categories

---
+++ src/api/categories/categories-mock.ts
@@ -0,0 +1,39 @@
+import { CategoryAttributes } from "./category-model";
+
+// Mock data for the categories
+const categoriesMock: CategoryAttributes[] = [
+    {
+        category_id: "technology",
+        label: "Technology"
+    },
+    {
+        category_id: "software",
+        label: "Software"
+    },
+    {
+        category_id: "it",
+        label: "IT"
+    },
+    {
+        category_id: "hardware",
+        label: "hardware"
+    },
+    {
+        category_id: "art",
+        label: "Art"   
+    },
+    {
+        category_id: "painting",
+        label: "Painting"   
+    },
+    {
+        category_id: "graphical-pictures",
+        label: "Graphical pictures"   
+    },
+    {
+        category_id: "handmade-works",
+        label: "Handmade works"   
+    }
+]
+
+export default categoriesMock;
