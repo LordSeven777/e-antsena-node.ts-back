5c12c1aaef71075435277bad9972b9ce126cae44
Tolotra <tolotor@yahoo.fr>
2022-03-12 19:25:13 +0300

    Export of category model and mock data from categories entry point

---
+++ src/api/categories/index.ts
@@ -0,0 +1,12 @@
+// Category model
+import CategoryModel, { CategoryAttributes, categoryModelSchema } from "./category-model";
+
+// Mock data for categories
+import categoriesMock from "./categories-mock";
+
+export {
+    CategoryModel,
+    CategoryAttributes,
+    categoryModelSchema,
+    categoriesMock
+};
