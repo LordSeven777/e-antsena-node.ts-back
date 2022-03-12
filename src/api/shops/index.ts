2b5acbf506802c82026e5c6f7ffeb6907a02448b
Tolotra <tolotor@yahoo.fr>
2022-03-12 10:29:08 +0300

    Export of shop model and mock data from shops api entry point

---
+++ src/api/shops/index.ts
@@ -0,0 +1,7 @@
+// Shop model
+import ShopModel, { ShopAttributes, ShopCreationAttributes, shopModelSchema } from "./shop-model";
+
+// Mock data for shops
+import shopsMock from "./shops-mock";
+
+export { ShopModel, ShopAttributes, ShopCreationAttributes, shopModelSchema, shopsMock };
