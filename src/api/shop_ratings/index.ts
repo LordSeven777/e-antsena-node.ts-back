045d06e49d22957197e2cbd9bfb426aa15009539
Tolotra <tolotor@yahoo.fr>
2022-03-12 12:48:26 +0300

    Export shop rating model and mock data from entry point

---
+++ src/api/shop_ratings/index.ts
@@ -0,0 +1,17 @@
+// Shop rating model
+import ShopRatingModel, {
+    ShopRatingAttributes,
+    ShopRatingCreationAttributes,
+    shopRatingModelSchema
+} from "./shop-rating-model";
+
+// Mock data for shop ratings
+import shopRatingsMock from "./shop-ratings-mock";
+
+export {
+    ShopRatingModel,
+    ShopRatingAttributes,
+    ShopRatingCreationAttributes,
+    shopRatingModelSchema,
+    shopRatingsMock
+};
