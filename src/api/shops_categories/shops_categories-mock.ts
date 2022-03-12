659cdacfb0de2c0ffae33f36b1c9dabaca46bb69
Tolotra <tolotor@yahoo.fr>
2022-03-12 20:36:28 +0300

    Mock data for the shops_categories relations

---
+++ src/api/shops_categories/shops_categories-mock.ts
@@ -0,0 +1,39 @@
+import { ShopsCategoriesAttributes } from "./shops_categories-model";
+
+// Mock data for the shops_categories relations
+const shopsCategoriesMock: ShopsCategoriesAttributes[] = [
+    {
+        shop_id: 1,
+        category_id: "technology"
+    },
+    {
+        shop_id: 1,
+        category_id: "software"
+    },
+    {
+        shop_id: 1,
+        category_id: "it"
+    },
+    {
+        shop_id: 1,
+        category_id: "hardware"
+    },
+    {
+        shop_id: 2,
+        category_id: "art"
+    },
+    {
+        shop_id: 2,
+        category_id: "painting"
+    },
+    {
+        shop_id: 2,
+        category_id: "graphical-pictures"
+    },
+    {
+        shop_id: 2,
+        category_id: "handmade-works"   
+    }
+];
+
+export default shopsCategoriesMock;
