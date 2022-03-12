37cddb92d026c5fa6b2e2275be5e0e40e22864c0
Tolotra <tolotor@yahoo.fr>
2022-03-12 09:30:23 +0300

    Mock data for users

---
+++ src/api/users/users-mock.ts
@@ -0,0 +1,32 @@
+// Mock data for users
+const usersMock = [
+    {
+        firstname: "Tolojanahary Fenomanantsoa",
+        lastname: "RABEFALY",
+        gender: "M",
+        photo_url: null,
+        email: "tolotor@yahoo.fr",
+        password: "ghyuhgfjidskbhgniuhgfhg",
+        role: "super_admin"
+    },
+    {
+        firstname: "Océane Tsiky Nofy",
+        lastname: "RAKOTOMAVO",
+        gender: "F",
+        photo_url: null,
+        email: "oceane_rak@gmail.com",
+        password: "uyighrbgfdgbrnogjhofdig",
+        role: "basic"
+    },
+    {
+        firstname: "Miaramanjaka Satu",
+        lastname: "RAMARIJAONA",
+        gender: "M",
+        photo_url: null,
+        email: "miara_satu@gmail.com",
+        password: "uhgeirbiugbfjkgbisufgjkfdsgbkfg",
+        role: "basic"
+    }
+]
+
+export default usersMock;
