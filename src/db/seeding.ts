9a297f1f8b4efbf4bb99f28da1fe34c40c73b9a9
Tolotra <tolotor@yahoo.fr>
2022-03-12 12:48:53 +0300

    Seeding the shop ratings table

--- src/db/seeding.ts
+++ src/db/seeding.ts
@@ -1,11 +1,13 @@
 // All models and mock data
 import { UserModel, usersMock } from "../api/users";
 import { ShopModel, shopsMock } from "../api/shops";
+import { ShopRatingModel, shopRatingsMock } from "../api/shop_ratings";
 
 // Seeds all tables with mock data
 const seedTables = async () => {
     await UserModel.bulkCreate(usersMock);
     await ShopModel.bulkCreate(shopsMock);
+    await ShopRatingModel.bulkCreate(shopRatingsMock);
 }
 
 export default seedTables;
