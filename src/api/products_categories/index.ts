b5585431fcab3c05e455530b2371d6ead5008da3
Tolotra <tolotor@yahoo.fr>
2022-03-13 10:48:39 +0300

    Export of the ProductsCategories model and mock data

---
+++ src/api/products_categories/index.ts
@@ -0,0 +1,15 @@
+// ProductsCategories model
+import ProductsCategoriesModel, { 
+    ProductsCategoriesAttributes,
+    productsCategoriesModelSchema
+} from "./products_categories-model";
+
+// Mock data for the products-categories relations
+import productsCategoriesMock from "./products_categories-mock";
+
+export {
+    ProductsCategoriesModel,
+    ProductsCategoriesAttributes,
+    productsCategoriesModelSchema,
+    productsCategoriesMock
+};
