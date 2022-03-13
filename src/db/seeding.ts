9fb6fa2c1337ec34253d37e2de43487cc0bd1de3
Tolotra <tolotor@yahoo.fr>
2022-03-13 10:50:46 +0300

    Seeding the products_categories table

--- src/db/seeding.ts
+++ src/db/seeding.ts
@@ -5,6 +5,7 @@ import { ShopRatingModel, shopRatingsMock } from "../api/shop_ratings";
 import { CategoryModel, categoriesMock } from "../api/categories";
 import { ShopsCategoriesModel, shopsCategoriesMock } from "../api/shops_categories";
 import { ProductModel, productsMock } from "../api/products";
+import { ProductsCategoriesModel, productsCategoriesMock } from "../api/products_categories";
 
 // Seeds all tables with mock data
 const seedTables = async () => {
@@ -14,6 +15,7 @@ const seedTables = async () => {
     await CategoryModel.bulkCreate(categoriesMock);
     await ShopsCategoriesModel.bulkCreate(shopsCategoriesMock);
     await ProductModel.bulkCreate(productsMock);
+    await ProductsCategoriesModel.bulkCreate(productsCategoriesMock);
 }
 
 export default seedTables;
