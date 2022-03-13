7ab165b39c963557ccd8fc5fdea96648a037f88d
Tolotra <tolotor@yahoo.fr>
2022-03-13 09:50:39 +0300

    Export of product model definition and mock data

---
+++ src/api/products/index.ts
@@ -0,0 +1,17 @@
+// Product model
+import ProductModel, {
+    ProductAttributes,
+    ProductCreationAttributes,
+    productModelSchema
+} from "./product-model";
+
+// Mock data for products
+import productsMock from "./products-mock";
+
+export {
+    ProductModel,
+    ProductAttributes,
+    ProductCreationAttributes,
+    productModelSchema,
+    productsMock
+};
