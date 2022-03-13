99fa9963db4288b576bef457929ba6a0602ff554
Tolotra <tolotor@yahoo.fr>
2022-03-13 10:47:51 +0300

    Mock data for the product-categories relations

---
+++ src/api/products_categories/products_categories-mock.ts
@@ -0,0 +1,55 @@
+import { ProductsCategoriesAttributes } from "./products_categories-model";
+
+// Mock data for the products-categories relations
+const productsCategoriesMock: ProductsCategoriesAttributes[] = [
+    {
+        product_id: 1,
+        category_id: "hardware"
+    },
+    {
+        product_id: 1,
+        category_id: "laptop"
+    },
+    {
+        product_id: 2,
+        category_id: "it"
+    },
+    {
+        product_id: 2,
+        category_id: "online-course"
+    },
+    {
+        product_id: 3,
+        category_id: "software"
+    },
+    {
+        product_id: 3,
+        category_id: "technology"
+    },
+    {
+        product_id: 4,
+        category_id: "art"
+    },
+    {
+        product_id: 4,
+        category_id: "painting"
+    },
+    {
+        product_id: 5,
+        category_id: "art"
+    },
+    {
+        product_id: 5,
+        category_id: "drawing"
+    },
+    {
+        product_id: 6,
+        category_id: "photography"
+    },
+    {
+        product_id: 6,
+        category_id: "graphical-pictures"
+    }
+];
+
+export default productsCategoriesMock;
