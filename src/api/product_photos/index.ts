5eae648ca40a76af2317a56b4dbff3579c8e361d
Tolotra <tolotor@yahoo.fr>
2022-03-13 22:26:12 +0300

    Export of the product photo model and mock data

---
+++ src/api/product_photos/index.ts
@@ -0,0 +1,17 @@
+// Product photo model
+import ProductPhotoModel, {
+    ProductPhotoAttributes,
+    ProductPhotoCreationAttributes,
+    productPhotoModelSchema
+} from "./product_photo-model";
+
+// Mock data for product photos
+import productPhotosMock from "./product_photos-mock";
+
+export {
+    ProductPhotoModel,
+    ProductPhotoAttributes,
+    ProductPhotoCreationAttributes,
+    productPhotoModelSchema,
+    productPhotosMock
+};
