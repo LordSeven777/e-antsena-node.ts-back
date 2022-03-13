861f7df5b714b76124c825076784f1a5dc60c06c
Tolotra <tolotor@yahoo.fr>
2022-03-13 23:25:14 +0300

    Export of the product rating model and mock data

---
+++ src/api/product_ratings/index.ts
@@ -0,0 +1,17 @@
+// Product rating model
+import ProductRatingModel, {
+    ProductRatingAttributes,
+    ProductRatingCreationAttributes,
+    productRatingModelSchema
+} from "./product_rating-model";
+
+// Mock data for product ratings
+import productRatingsMock from "./product_ratings-mock";
+
+export {
+    ProductRatingModel,
+    ProductRatingAttributes,
+    ProductRatingCreationAttributes,
+    productRatingModelSchema,
+    productRatingsMock
+}
