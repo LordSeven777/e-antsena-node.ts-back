689e104bce4d3c0aa75bd9b8c7090e82d380392b
Tolotra <tolotor@yahoo.fr>
2022-05-02 15:13:45 +0300

    Fix in the express middleware type

--- src/types/ExpressMiddleware-type.ts
+++ src/types/ExpressMiddleware-type.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 
 // Type for an express middleware
-type ExpressMiddleware = (req: Request, res: Response, next: NextFunction) => void | ErrorRequestHandler;
+type ExpressMiddleware = ((req: Request, res: Response, next: NextFunction) => void) | ErrorRequestHandler;
 
 export default ExpressMiddleware;
