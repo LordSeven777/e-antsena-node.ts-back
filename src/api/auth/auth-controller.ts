c54de94c5abdfb24d02d9957d3e41498dd38088a
Tolotra <tolotor@yahoo.fr>
2022-05-04 23:50:34 +0300

    Auth controller: Login finished

--- src/api/auth/auth-controller.ts
+++ src/api/auth/auth-controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
+// Response error helper
+import ResponseError from "../../helpers/ResponseError-helper";
+
 // Services
 import authService from "./auth-service";
 import { usersService } from "../users";
@@ -58,7 +61,33 @@ class AuthController {
     // User login
     async userLogin(req: Request, res: Response, next: NextFunction) {
         try {
-            res.send("User logged in");
+            const { email, password } = req.body;
+
+            const authUser = await usersService.findUserByEmailAndPassword(email, password);
+            if (!authUser) return next(new ResponseError(400, "The email or password is wrong"));
+
+            // Deleting the password for security purposes
+            delete authUser["password"];
+
+            /* The user needs owned shop information after login */
+            const userShop = await usersService.getUserShop(authUser.userId);
+
+            // User token payload
+            const userTokenPayload: UserTokenPayload = {
+                userId: authUser.userId,
+                shopId: userShop?.shopId || null
+            }
+            // Getting the tokens
+            const accessToken = await authService.generateUserToken(userTokenPayload, "access");
+            const refreshToken = await authService.generateUserToken(userTokenPayload, "refresh");
+            // Storing the refresh token
+            await authService.storeRefreshToken(refreshToken);
+
+            res.json({
+                user: { ...authUser, shop: userShop },
+                accessToken,
+                refreshToken
+            });
         }
         catch (error) {
             next(error);
